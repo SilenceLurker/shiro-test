2ce7a55dc57e01f4169bbee9fce0b1365780e846
SilenceLurker <78900386+SilenceLurker@users.noreply.github.com>
2024-04-24 14:02:41 +0800

    init

---
+++ src/main/java/xyz/silencelurker/test/shiro/ShiroApplication.java
@@ -0,0 +1,16 @@
+package xyz.silencelurker.test.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Silence_Lurker
+ */
+@SpringBootApplication
+public class ShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroApplication.class, args);
+	}
+
+}
---
+++ src/main/java/xyz/silencelurker/test/shiro/entity/UserInfo.java
@@ -0,0 +1,11 @@
+package xyz.silencelurker.test.shiro.entity;
+
+import lombok.Data;
+
+/**
+ * @author Silence_Lurker
+ */
+@Data
+public class UserInfo {
+
+}
